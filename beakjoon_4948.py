c4646e1503be0fb5a0be497808a65c2e28084e20
w00ng <56468120+JIW00NG@users.noreply.github.com>
2020-08-04 23:56:59 +0900

    12th

--- beakjoon_4948.py
+++ beakjoon_4948.py
@@ -18,3 +18,31 @@
 # 출력
 # 각 테스트 케이스에 대해서, n보다 크고, 2n보다 작거나 같은 소수의 개수를 출력한다.
 
+def prime_list(n):
+    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+    sieve = [True] * n
+    # print(sieve)
+
+    # n의 최대 약수가 sqrt(n) 이하이므로 i=sqrt(n)까지 검사
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i] == True:           # i가 소수인 경우
+            for j in range(i+i, n, i): # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+
+    # 소수 목록 산출
+    return [i for i in range(2, n) if sieve[i] == True]
+
+
+import sys
+
+
+while True:
+    n = int(sys.stdin.readline())
+    count = 0
+    if n == 0:
+        break
+    for i in prime_list(2 * n + 1):
+        if i > n:
+            count = count + 1
+    print(count)
