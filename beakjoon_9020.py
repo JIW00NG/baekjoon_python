0f3b448225ef186245d0681598d7f3d31dc7d296
w00ng <56468120+JIW00NG@users.noreply.github.com>
2020-08-05 23:32:07 +0900

    12th

--- beakjoon_9020.py
+++ beakjoon_9020.py
@@ -13,3 +13,26 @@
 #
 # 출력
 # 각 테스트 케이스에 대해서 주어진 n의 골드바흐 파티션을 출력한다. 출력하는 소수는 작은 것부터 먼저 출력하며, 공백으로 구분한다.
+
+# n의 최대 약수가 sqrt(n) 이하이므로 i=sqrt(n)까지 검사
+
+def prime_list(n): # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+    # sieve = [True] * n
+
+    # n의 최대 약수가 sqrt(n) 이하이므로 i=sqrt(n)까지 검사
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i] == True:           # i가 소수인 경우
+            for j in range(i+i, n, i): # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+
+    # 소수 목록 산출
+    return [i for i in range(2, n) if sieve[i] == True]
+
+import sys
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    n = int(sys.stdin.readline())
+
